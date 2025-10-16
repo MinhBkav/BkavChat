f68f09848ad658aa879e4c3b89ddf2c727c3dc67
Geniedev1 <MinhDN.B22CN528@stu.ptit.edu.vn>
2025-10-16 13:44:12 +0700

    refactor code

--- socket/HandlerMessage/HistoryMessage.js
+++ socket/HandlerMessage/HistoryMessage.js
@@ -16,6 +16,7 @@ module.exports = (io,socket) =>{
         }
 
         let query = {};
+        // Áp dụng mốc thời gian "before" (nếu có)
         if (before) {
           query.CreatedAt = { $lt: new Date(before) };
         }
